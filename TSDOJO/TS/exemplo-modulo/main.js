95471dea7f5733c4cfaf069c515a570110533f33
Majo <maria.correia.s@gmail.com>
2019-09-06 11:46:59 -0300

    Exemplo de modulo 'import' e 'Export'

---
+++ TSDOJO/TS/exemplo-modulo/main.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var Pessoa_1 = require("./classes/Pessoa");
+var pessoa = new Pessoa_1.Pessoa();
+pessoa.nome = "Majo";
+pessoa.email = "Silva";
+console.log(pessoa);
---
+++ TSDOJO/TS/exemplo-modulo/main.ts
@@ -0,0 +1,7 @@
+import { Pessoa } from "./classes/Pessoa";
+
+let pessoa = new Pessoa();
+pessoa.nome =  "Majo";
+pessoa.email = "Silva";
+
+console.log(pessoa);

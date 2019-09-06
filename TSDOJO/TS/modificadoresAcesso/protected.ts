6b86e3ce9f3019cb1b5a441830a7c99012f9d9f1
Majo <maria.correia.s@gmail.com>
2019-09-05 22:28:06 -0300

    Modificador de acesso protegido

---
+++ TSDOJO/TS/modificadoresAcesso/protected.ts
@@ -0,0 +1,23 @@
+class Pessoa{
+    protected nome: string;
+    constructor(nome: string){
+        this.nome = nome;
+    }
+}
+
+class Funcionario extends Pessoa{
+    private setor: string;
+
+    constructor(nome: string, setor: string){
+        super(nome);
+        this.setor = setor;
+    }
+
+    public apresentacao(){
+        return `Olá! Meu nome é ${this.nome} e eu trabalho no setor ${this.setor}.`;
+    }
+}
+
+let func = new Funcionario("Jose", "Vendas");
+console.log(func.apresentacao());
+//console.log(jose.nome);//error
